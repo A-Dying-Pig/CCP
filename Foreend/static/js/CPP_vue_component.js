d2cb0bfda9147fc0eb84117b9b0aa12717dce00b
A-Dying-Pig <32242762+A-Dying-Pig@users.noreply.github.com>
2018-11-09 15:56:52 +0800

    Vue组件 比赛介绍卡片

--- Foreend/static/js/CPP_vue_component.js
+++ Foreend/static/js/CPP_vue_component.js
@@ -86,3 +86,47 @@ Vue.component('register-input',{
         },
 });
 
+//competition info
+Vue.component("competition-profile",{
+    props:{
+        w:{
+            type:Number,
+            default:200,
+        },
+        h:{
+            type:Number,
+            default:400,
+        },
+        img_url: {
+            type: String,
+            default: ''
+        },
+        title:{
+            type:String,
+            default:''
+        },
+        intro:{
+            type:String,
+            default:''
+        },
+        comp_url:{
+            type:String,
+            default:''
+        }
+    },
+    template:
+        `
+            <div class="competition-profile-unit"
+                 :style="{width:w +'px'}"
+                 style="white-space:normal;word-break:break-all;text-overflow: ellipsis"
+            >
+                <img
+                    v-bind:src=img_url
+                    :style="{width:w+'px',height:w+'px'}"
+                >
+                <br>
+                <span><center>{{title}}</center></span>
+                <p>{{intro}}</p>
+            </div>
+        `,
+});
