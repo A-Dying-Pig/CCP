04e21eec3c7fdcee92500e8ed112fde8bfb7678d
ZYJ-JMF <zyjjmf323@126.com>
2018-11-12 10:58:00 +0800

    add competition list

--- Foreend/static/js/CPP_vue_component.js
+++ Foreend/static/js/CPP_vue_component.js
@@ -342,3 +342,15 @@ Vue.component('register-wordlimit',{
         }
     }
 });
+
+Vue.component('competition-list',{
+    props:['com'],
+    template:`
+    <div class="competition-list">
+        <td>{{com.number}}</td>
+        <td>{{com.name}}</td>
+        <td>{{com.organizer}}</td>
+        <td>{{com.information}}</td>
+        <td>{{com.detail}}</td>  
+    </div>`
+});
