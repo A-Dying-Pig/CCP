0dd68d6321a77050d9ed3514fd52635c69888314
A-Dying-Pig <32242762+A-Dying-Pig@users.noreply.github.com>
2018-11-13 17:26:22 +0800

    使用element-ui添加了上传文件、地区选择组件

---
+++ Foreend/src/components/RegionPicker.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class = "region-picker">
+        <label class="region-picker-label">{{label}} </label>
+        <v-distpicker  only-province @selected="onSelected"></v-distpicker>
+    </div>
+</template>
+
+
+<script>
+    import VDistpicker from 'v-distpicker'
+
+    export default {
+        name:"RegionPicker",
+        data:function(){
+            return{
+                province:null,
+            }
+        },
+        props:{
+            label:{
+                default:"工作省份:",
+                type:String
+            }
+        },
+        components: { VDistpicker },
+        methods: {
+            onSelected:function(data) {
+                this.province = data.province.value;
+            },
+          }
+    }
+</script>
+
+<style>
+    .region-picker{
+        width:600px;
+    }
+
+    .region-picker-label{
+        font-size:16px;
+    }
+</style>
