c66735341f1d48a8df37482804dd2c092ada7a7d
htx <beyondhtx@163.com>
2018-11-13 18:10:26 +0800

    init database

--- Backend/models.py
+++ Backend/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class CCPUser(AbstractUser):
+    # id = models.AutoField(primary_key=True)
+    email = models.EmailField()
+    gender = models.BooleanField()
+    birthday = models.DateTimeField(blank=True)
+    introduction = models.CharField(max_length=50, blank=True)
+
+
+class Contest(models.Model):
+    title = models.CharField(max_length=20)
+    status = models.IntegerField()  # to be extended
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    enroll_start = models.DateTimeField()
+    enroll_end = models.DateTimeField()
+    information = models.TextField()
+    organiser_id = models.IntegerField()  # User表里的id
+    # organiser = models.ForeignKey(User, on_delete=models.PROTECT)
+
+
+class JudgeRelation(models.Model):
+    competitor_id = models.IntegerField(db_index=True)
+    contest_id = models.IntegerField(db_index=True)
+    judge_id = models.IntegerField(default=-1, db_index=True)
+    grade = models.IntegerField(default=-1)
+
+
+class ContestJudge(models.Model):
+    judge_id = models.IntegerField(db_index=True)
+    contest_id = models.IntegerField(db_index=True)
--- Kernal/settings.py
+++ Kernal/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Backend'
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -76,8 +77,12 @@ WSGI_APPLICATION = 'Kernal.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ccptest',
+        'USER': 'beyondhtx',
+        'PASSWORD': '13081053',
+        'HOST': '127.0.0.1',
+        'PORT': '3306'
     }
 }
 
@@ -119,3 +124,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+AUTH_USER_MODEL = 'Backend.CCPUser'
