98b420498454209c23147c9c7b8679d0e063f6d1
DingZheng2016 <dingzheng214@126.com>
2018-11-16 16:30:09 +0800

    fix authentication bug

--- Kernal/urls.py
+++ Kernal/urls.py
@@ -6,10 +6,11 @@ import Foreend.views
 urlpatterns = [
     # url(r'^admin/', admin.site.urls),
     path(r'', Foreend.views.index),
+    path(r'index', Foreend.views.index),
     path(r'login', Foreend.views.login),
     path(r'register', Foreend.views.register),
     path(r'logout', Foreend.views.logout),
-    path(r'contests', Foreend.views.contest),
+    path(r'contest', Foreend.views.contest),
     path(r'detail', Foreend.views.detail),
     path(r'enroll', Foreend.views.enroll),
     path(r'profile', Foreend.views.profile),
