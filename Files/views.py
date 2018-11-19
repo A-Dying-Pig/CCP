5ade990c542d69a37031060071403ec6c4a78d13
DingZheng2016 <dingzheng214@126.com>
2018-11-20 00:19:35 +0800

    add download files

--- Files/views.py
+++ Files/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import StreamingHttpResponse
+import io
 
-# Create your views here.
+def file_iterator(file_name):
+    f = io.open(file_name, 'rb')
+    c = f.read() 
+    f.close()  
+    return c
+
+def download(request):
+
+    the_file_name =  request.path
+    response = StreamingHttpResponse(file_iterator(the_file_name))
+    response['Content-Disposition']='attachment;filename="{0}"'.format(path.split('/')[-1])
+    return response
--- Kernal/urls.py
+++ Kernal/urls.py
@@ -1,7 +1,8 @@
 
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.contrib import admin
 import Foreend.views
+import Files.views
 
 urlpatterns = [
     path(r'', Foreend.views.index),
@@ -15,4 +16,5 @@ urlpatterns = [
     path(r'profile', Foreend.views.profile),
     path(r'addContest', Foreend.views.addContest),
     path(r'api/', include('Backend.urls')),
+    re_path(r'^resources/', Files.views.download),
 ]
