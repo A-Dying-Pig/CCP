525db2281e4dce6b1c473cfd97a18ff3c4ff9df4
DingZheng2016 <dingzheng214@126.com>
2018-11-20 23:28:56 +0800

    modify superadmin to super, a strange naming bug

--- Foreend/views.py
+++ Foreend/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.decorators import login_required
 from django.contrib import auth
 
