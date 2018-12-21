8a94ad129b86b3abdb6d70f78d31708f94484edf
A-Dying-Pig <32242762+A-Dying-Pig@users.noreply.github.com>
2018-12-21 17:22:30 +0800

    创建比赛功能测试,个人中心部分功能测试

--- Foreend/tests/function/page.py
+++ Foreend/tests/function/page.py
@@ -3,6 +3,7 @@ import locators
 from element import BaseInput
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 import settings
 
 class BasePage(object):
@@ -91,6 +92,8 @@ class NavigationBar(BasePage):
         return self.click_index() and self.click_all_contests() and self.click_personal_center() \
                and self.click_create_contest() and self.click_login() and self.click_register()
 
+    def click_message_bell(self):
+        pass
 
 #Contests List
 class AllContests(BasePage):
@@ -116,25 +119,22 @@ class AllContests(BasePage):
         elements = self.driver.find_elements(*locators.AllContestsLocators.TypeSelector)
         for element in elements:
             element.click()
-            if(len(self.driver.find_elements(*locators.AllContestsLocators.ContestsButton)) > length):
+            if len(self.driver.find_elements(*locators.AllContestsLocators.ContestsButton)) > length:
                 return False
         return True
 
 
 #Login
-
 class Login(BasePage):
 
     def login(self,usr,pwd):
         inputs = self.driver.find_elements(*locators.LoginLocators.Input)
-        username = BaseInput(inputs[0])
-        password = BaseInput(inputs[1])
-        username.input = usr
-        password.input = pwd
+        BaseInput(inputs[0]).input = usr
+        BaseInput(inputs[1]).input = pwd
         self.driver.find_element(*locators.LoginLocators.LoginButton).click()
         try:
             WebDriverWait(self.driver, settings.configs['MAX_WAITING_TIME']).until(
-                EC.visibility_of_element_located(locators.LoginLocators.NonExist)
+                EC.visibility_of_element_located(locators.BaseLocators.NonExist)
             )
         except:
             print('Login Wait')
@@ -143,4 +143,128 @@ class Login(BasePage):
     def click_create_new_account(self):
         self.driver.find_element(*locators.LoginLocators.CreateAccountLink).click()
         return '创建新账户' in self.driver.title
+
+
+#Register
+class Register(BasePage):
+    def fill_form(self,eml,usr,pwd):
+        forms = self.driver.find_elements(*locators.RegisterLocators.FormInputs)
+        BaseInput(forms[0]).input = eml
+        BaseInput(forms[1]).input = usr
+        BaseInput(forms[2]).input = pwd
+        BaseInput(forms[3]).input = pwd
+
+    def click_submit(self):
+        self.driver.find_elements(*locators.RegisterLocators.FormButtons)[0].click()
+        element = None
+        try:
+            element = WebDriverWait(self.driver, settings.configs['MAX_WAITING_TIME']).until(
+                EC.visibility_of_element_located(locators.RegisterLocators.AlertMessage)
+            )
+            print('register fail :'+ element.text)
+        except:
+            print('register success')
+        return element or '主页' in self.driver.title
+
+    def click_reset(self):
+        self.driver.find_elements(*locators.RegisterLocators.FormButtons)[1].click()
+
+
+#Personal Center
+class PersonalCenter(BasePage):
+    def click_menu_contests(self):
+        self.driver.find_element(*locators.PersonalCenterLocators.MenuContests).click()
+
+    def click_menu_info(self):
+        self.driver.find_element(*locators.PersonalCenterLocators.MenuInfo).click()
+
+    def click_menu_messages(self):
+        self.driver.find_element(*locators.PersonalCenterLocators.MenuMessage).click()
+
+    def click_tab_participate(self):
+        self.driver.find_element(*locators.PersonalCenterLocators.TabParticipate).click()
+
+    def click_tab_create(self):
+        self.driver.find_element(*locators.PersonalCenterLocators.TabCreate).click()
+
+    def click_tab_judge(self):
+        self.driver.find_element(*locators.PersonalCenterLocators.TabJudge).click()
+
+
+
+#Create a competition
+class CreateContest(BasePage):
+    def fill_contest_name(self,name):
+        BaseInput(self.driver.find_element(*locators.CreateContest.ContestName)).input = name
+
+    def fill_holders(self,names):
+        length = len(names)
+        for i in range(0,length - 1):
+            self.driver.find_element(*locators.CreateContest.HoldersPlus).click()
+        elements = self.driver.find_elements(*locators.CreateContest.HoldersInputs)
+        for i in range(0,length):
+            elements[i].send_keys(names[i])
+
+    def fill_sponsors(self,names):
+        length = len(names)
+        if length == 0:
+            return
+        for i in range(0,length):
+            self.driver.find_element(*locators.CreateContest.SponsorsPlus).click()
+        elements = self.driver.find_elements(*locators.CreateContest.SponsorsInputs)
+        for i in range(0,length):
+            elements[i].send_keys(names[i])
+
+    def choose_contest_type(self,index):
+        self.driver.find_elements(*locators.CreateContest.ContestsType)[index].click()
+
+    def upload_contest_image(self,file_path):
+        self.driver.find_element(*locators.CreateContest.ContestsImage).send_keys(file_path)
+
+    def fill_contest_intro(self,brief_intro,detail_intro):
+        self.driver.find_element(*locators.CreateContest.BriefIntroduction).send_keys(brief_intro)
+        self.driver.find_element(*locators.CreateContest.DetailIntroduction).send_keys(detail_intro)
+
+
+    #Y-m-d H:min:S
+    def fill_enroll_time(self,start_time,end_time):
+        self.driver.find_element(*locators.CreateContest.EnrollStartTime).send_key(start_time)
+        self.driver.find_element(*locators.CreateContest.EnrollStartTime).send_key(end_time)
+
+    def set_contest_type_person(self):
+        self.driver.find_element(*locators.CreateContest.PersonalContestButton).click()
+
+    def set_contest_type_group(self):
+        self.driver.find_element(*locators.CreateContest.GroupContestButton).click()
+
+    def fill_person_needed_info(self,info):
+        length = len(info)
+        if length == 0:
+            return
+        for i in range(0, length - 1):
+            self.driver.find_element(*locators.CreateContest.PersonInfoPlus).click()
+        elements = self.driver.find_elements(*locators.CreateContest.PersonInfoInputs)
+        for i in range(0, length):
+            elements[i].send_keys(info[i])
+
+
+
+#pass a competition
+class Superadmin(BasePage):
+    pass
+
+
+#Enroll a competition
+class Enroll(BasePage):
+    pass
+
+
+
+
+
+
+
+
+
+
 
--- Foreend/tests/function/settings.py
+++ Foreend/tests/function/settings.py
@@ -1,3 +1,8 @@
+import os
+import datetime
+
+today = datetime.date.today()
+
 configs = {
     'BASE_URL': 'http://127.0.0.1:8000',
     'SUPERADMIN_URL': 'http://127.0.0.1:8000/superadmin',
@@ -8,9 +13,34 @@ configs = {
     'SUPERUSER_PASSWORD': '123456',
     'JUDGE_USERNAME': 'wzw',
     'JUDGE_PASSWORD': '123456',
+    'REGISTER_EMAIL': 'function_test@163.com',
+    'REGISTER_USERNAME': 'function_test_username',
+    'REGISTER_PASSWORD': '123456',
 
 
+    'CREATE_CONTEST':{
+        'CONTEST_NAME': '功能测试比赛',
+        'HOLDERS': ['清华大学','北京大学'],
+        'SPONSORS': ['阿里巴巴','腾讯'],
+        'CONTEST_TYPE_INDEX':0,
+        'DEFAULT_CONTEST_IMAGE' : os.path.join(os.getcwd(), 'test_files/contest_image.jpg'),
+        'BRIEF_INTRO': '功能测试比赛简介',
+        'DETAIL_INTRO': '功能测试比赛详细介绍',
+        #Enroll Time
+        'ENROLL_START_TIME': today.strftime('%Y-%m-%d 00:00:00'),
+        'ENROLL_END_TIME': today.replace(day = today.day + 1).strftime('%Y-%m-%d 00:00:00'),
+        #Stage Time
+        'STAGE_NUMBER': 2,
+        #stage1
+        'STAGE1_START_TIME':today.replace(day = today.day + 1).strftime('%Y-%m-%d 00:00:00'),
+        'STAGE1_SUBMIT_END_TIME':today.replace(day = today.day + 2).strftime('%Y-%m-%d 00:00:00'),
+        'STAGE1_JUDGE_END_TIME':today.replace(day = today.day + 3).strftime('%Y-%m-%d 00:00:00'),
+        #stage2
+        'STAGE2_START_TIME': today.replace(day=today.day + 3).strftime('%Y-%m-%d 00:00:00'),
+        'STAGE2_SUBMIT_END_TIME': today.replace(day=today.day + 4).strftime('%Y-%m-%d 00:00:00'),
+        'STAGE2_JUDGE_END_TIME': today.replace(day=today.day + 5).strftime('%Y-%m-%d 00:00:00'),
 
+    },
 }
 
 
